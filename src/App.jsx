0f269734dc4052b84ca968662ef45f6674ad0c5a
rabby-web <rabby.webdeveloper@gmail.com>
2024-01-13 12:13:10 +0600

    O and X add to board

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
-function Square() {
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
   return (
-    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-      X
+    <button
+      onClick={onSquareClick}
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+    >
+      {value}
     </button>
   );
 }
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
   return (
     <>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={() => handClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handClick(2)} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={() => handClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handClick(5)} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={() => handClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handClick(8)} />
       </div>
     </>
   );
